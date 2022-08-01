ef05591ef7f9923517718d610a812630951a0ecd
karolwk <krl@o2.pl>
2022-08-01 17:52:41 +0200

    added styling and navigation

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,14 +1,79 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
+import IconButton from '../components/UI/IconButton';
+import { GlobalStyles } from '../constants/styles';
+import Button from '../components/UI/Button';
 
-const ManageExpenses = () => {
+const ManageExpenses = ({ route, navigation }) => {
+  // With "?" after params we check if params are undefined
+  // if yes then we don't look up for another value in it
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId; // Another JS trick to convert value to boolean
+
+  useLayoutEffect(() => {
+    // Seting title dynamicly
+    navigation.setOptions({
+      title: isEditing ? 'Edit Expense' : 'Add Expense',
+    });
+  }, [navigation, isEditing]);
+
+  const deleteExpeneseHanlder = () => {
+    navigation.goBack(); // Go back to screen that opened this screen
+  };
+
+  const cancelHandler = () => {
+    navigation.goBack();
+  };
+
+  const confirmHandler = () => {
+    navigation.goBack();
+  };
   return (
-    <View>
-      <Text>ManageExpenses</Text>
+    <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button mode="flat" onPress={cancelHandler} style={styles.button}>
+          Cancel
+        </Button>
+        <Button onPress={confirmHandler} style={styles.button}>
+          {isEditing ? 'Update' : 'Add'}
+        </Button>
+      </View>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpeneseHanlder}
+          />
+        </View>
+      )}
     </View>
   );
 };
 
 export default ManageExpenses;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center',
+  },
+});
