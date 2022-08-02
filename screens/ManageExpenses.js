bb460794f785699b5512a3643817d1adc2cebec5
karolwk <krl@o2.pl>
2022-08-02 15:43:08 +0200

    added logic to handling managing expanses

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,14 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React, { useLayoutEffect } from 'react';
+import React, { useContext, useLayoutEffect } from 'react';
 import IconButton from '../components/UI/IconButton';
 import { GlobalStyles } from '../constants/styles';
 import Button from '../components/UI/Button';
+import { ExpensesContext } from '../store/expenses-context';
 
 const ManageExpenses = ({ route, navigation }) => {
   // With "?" after params we check if params are undefined
   // if yes then we don't look up for another value in it
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId; // Another JS trick to convert value to boolean
+
+  const expneseCtx = useContext(ExpensesContext);
 
   useLayoutEffect(() => {
     // Seting title dynamicly
@@ -18,6 +21,7 @@ const ManageExpenses = ({ route, navigation }) => {
   }, [navigation, isEditing]);
 
   const deleteExpeneseHanlder = () => {
+    expneseCtx.deleteExpense(editedExpenseId);
     navigation.goBack(); // Go back to screen that opened this screen
   };
 
@@ -26,6 +30,19 @@ const ManageExpenses = ({ route, navigation }) => {
   };
 
   const confirmHandler = () => {
+    if (isEditing) {
+      expneseCtx.updateExpense(editedExpenseId, {
+        description: 'TestUpdate',
+        amount: 29.99,
+        date: new Date('2022-07-29'),
+      });
+    } else {
+      expneseCtx.addExpense({
+        description: 'Test',
+        amount: 19.99,
+        date: new Date('2022-07-28'),
+      });
+    }
     navigation.goBack();
   };
   return (
